2382ce257ad6f1de4f3496f81de7b660a8dca49a
Lisa Williams <williams.lisarenee@gmail.com>
2016-11-14 22:19:08 -0500

    added firebase email/pass auth

--- js/new-age.js
+++ js/new-age.js
@@ -7,6 +7,55 @@
     messagingSenderId: "675537655730"
   };
   firebase.initializeApp(config);
+  
+// EMAIL/PASSWORD LOGIN VIA FIREBASE
+const auth = firebase.auth();
+auth.signInWithEmailAndPassword(email, pass);
+auth.createUserWithEmailAndPassword(email, pass);
+auth.onAuthStateChanged(firebaseUser => {})
+
+const loginEmail = document.getElementById('loginEmail');
+const loginPassword = document.getElementById('loginPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignup = document.getElementById('btnSignup');
+const btnLogOut = document.getElementById('btnLogOut')
+
+//ADD LOGIN EVENT 
+btnLogin.addEventListener('click', e => {
+    //GET EMAIL AND PASS
+    const email = loginEmail.value;
+    const pass = loginPassword.value;
+    const auth = firebase.auth();
+    //SIGN IN
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+//SIGN UP EVENT
+btnSignup.addEventListener('click', e => {
+    //GET EMAIL AND PASS
+    //TO DO: CHECK FOR REAL EMAILS (#FUTURE GOALS)
+    const email = loginEmail.value;
+    const pass = loginPassword.value;
+    const auth = firebase.auth();
+    //SIGN IN
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+//LOG OUT EVENT
+btnLogOut.addEventListener('click', e => {
+    firebase.auth().signOut();
+
+});
+
+//ADD REALTIME LISTENER 
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        console.log(firebaseUser);
+    } else {
+        console.log('not logged in');
+    }
+});
+
 
 (function($) {
     "use strict"; // Start of use strict
